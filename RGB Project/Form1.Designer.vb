85dacecd117fc7004d887d394b073c8d290a5c4a
Abhishek sharma <140407169+Abhishek-sharma23@users.noreply.github.com>
2025-08-26 11:05:03 +0530

    Add files via upload

---
+++ RGB Project/Form1.Designer.vb
@@ -0,0 +1,211 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        TextBox5 = New TextBox()
+        TextBox4 = New TextBox()
+        TextBox3 = New TextBox()
+        VScrollBar3 = New VScrollBar()
+        TextBox2 = New TextBox()
+        VScrollBar2 = New VScrollBar()
+        TextBox1 = New TextBox()
+        VScrollBar1 = New VScrollBar()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        SuspendLayout()
+        ' 
+        ' TextBox5
+        ' 
+        TextBox5.Location = New Point(574, 178)
+        TextBox5.Margin = New Padding(3, 4, 3, 4)
+        TextBox5.Name = "TextBox5"
+        TextBox5.Size = New Size(94, 27)
+        TextBox5.TabIndex = 15
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(574, 348)
+        TextBox4.Margin = New Padding(3, 4, 3, 4)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(94, 27)
+        TextBox4.TabIndex = 14
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(350, 449)
+        TextBox3.Margin = New Padding(3, 4, 3, 4)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(54, 27)
+        TextBox3.TabIndex = 13
+        ' 
+        ' VScrollBar3
+        ' 
+        VScrollBar3.Location = New Point(350, 108)
+        VScrollBar3.Maximum = 264
+        VScrollBar3.Name = "VScrollBar3"
+        VScrollBar3.ScaleScrollBarForDpiChange = False
+        VScrollBar3.Size = New Size(54, 319)
+        VScrollBar3.TabIndex = 12
+        VScrollBar3.TabStop = True
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(235, 449)
+        TextBox2.Margin = New Padding(3, 4, 3, 4)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(55, 27)
+        TextBox2.TabIndex = 11
+        ' 
+        ' VScrollBar2
+        ' 
+        VScrollBar2.Location = New Point(235, 108)
+        VScrollBar2.Maximum = 264
+        VScrollBar2.Name = "VScrollBar2"
+        VScrollBar2.ScaleScrollBarForDpiChange = False
+        VScrollBar2.Size = New Size(55, 319)
+        VScrollBar2.TabIndex = 10
+        VScrollBar2.TabStop = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(125, 449)
+        TextBox1.Margin = New Padding(3, 4, 3, 4)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(54, 27)
+        TextBox1.TabIndex = 9
+        ' 
+        ' VScrollBar1
+        ' 
+        VScrollBar1.Location = New Point(125, 108)
+        VScrollBar1.Maximum = 264
+        VScrollBar1.Name = "VScrollBar1"
+        VScrollBar1.ScaleScrollBarForDpiChange = False
+        VScrollBar1.Size = New Size(54, 319)
+        VScrollBar1.TabIndex = 8
+        VScrollBar1.TabStop = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.ForeColor = Color.Firebrick
+        Label1.Location = New Point(121, 53)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(53, 20)
+        Label1.TabIndex = 16
+        Label1.Text = "Label1"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.ForeColor = Color.ForestGreen
+        Label2.Location = New Point(228, 53)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(53, 20)
+        Label2.TabIndex = 17
+        Label2.Text = "Label2"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.ForeColor = Color.SkyBlue
+        Label3.Location = New Point(343, 53)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(53, 20)
+        Label3.TabIndex = 18
+        Label3.Text = "Label3"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.ForeColor = Color.FromArgb(CByte(192), CByte(0), CByte(192))
+        Label4.Location = New Point(528, 154)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(53, 20)
+        Label4.TabIndex = 19
+        Label4.Text = "Label4"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.ForeColor = Color.FromArgb(CByte(192), CByte(0), CByte(192))
+        Label5.Location = New Point(528, 324)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(53, 20)
+        Label5.TabIndex = 20
+        Label5.Text = "Label5"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.ForeColor = Color.FromArgb(CByte(192), CByte(0), CByte(192))
+        Label6.Location = New Point(21, 456)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(53, 20)
+        Label6.TabIndex = 21
+        Label6.Text = "Label6"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(893, 553)
+        Controls.Add(Label6)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(TextBox5)
+        Controls.Add(TextBox4)
+        Controls.Add(TextBox3)
+        Controls.Add(VScrollBar3)
+        Controls.Add(TextBox2)
+        Controls.Add(VScrollBar2)
+        Controls.Add(TextBox1)
+        Controls.Add(VScrollBar1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents VScrollBar3 As VScrollBar
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents VScrollBar2 As VScrollBar
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents VScrollBar1 As VScrollBar
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+
+End Class
---
+++ RGB Project/Form1.vb
@@ -0,0 +1,153 @@
+﻿
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Label1.Text = "Red Color
+    (R)"
+        Label2.Text = "Green Color
+    (G)"
+        Label3.Text = "Blue Color
+    (B)"
+        Label4.Text = "3 Digit Hexadecimal Value"
+        Label5.Text = "6 Digit Hexadecimal Value"
+        Label6.Text = "RGB Values"
+        BackColor = Color.Black
+        TextBox1.Enabled = False
+        TextBox2.Enabled = False
+        TextBox3.Enabled = False
+        TextBox1.Text = VScrollBar1.Value
+        TextBox2.Text = VScrollBar2.Value
+        TextBox3.Text = VScrollBar3.Value
+        hexconvert(VScrollBar1.Value, VScrollBar2.Value, VScrollBar3.Value)
+
+    End Sub
+
+    Private Sub VScrollBar1_Scroll(sender As Object, e As ScrollEventArgs) Handles VScrollBar1.Scroll
+        Me.BackColor = Color.FromArgb(VScrollBar1.Value, VScrollBar2.Value, VScrollBar3.Value)
+        TextBox1.Text = VScrollBar1.Value.ToString()
+        hexconvert(VScrollBar1.Value, VScrollBar2.Value, VScrollBar3.Value)
+    End Sub
+
+    Private Sub VScrollBar2_Scroll(sender As Object, e As ScrollEventArgs) Handles VScrollBar2.Scroll
+        Me.BackColor = Color.FromArgb(VScrollBar1.Value, VScrollBar2.Value, VScrollBar3.Value)
+        TextBox2.Text = VScrollBar2.Value.ToString()
+        hexconvert(VScrollBar1.Value, VScrollBar2.Value, VScrollBar3.Value)
+    End Sub
+
+    Private Sub VScrollBar3_Scroll(sender As Object, e As ScrollEventArgs) Handles VScrollBar3.Scroll
+        Me.BackColor = Color.FromArgb(VScrollBar1.Value, VScrollBar2.Value, VScrollBar3.Value)
+        TextBox3.Text = VScrollBar3.Value.ToString()
+        hexconvert(VScrollBar1.Value, VScrollBar2.Value, VScrollBar3.Value)
+    End Sub
+
+    Public Sub hexconvert(ByVal r As Integer, ByVal g As Integer, ByVal b As Integer)
+        Dim hexColor As String = "#"
+        Dim hexThree As String = "#"
+
+        hexColor &= DecimalToHexa6(r)
+        hexColor &= DecimalToHexa6(g)
+        hexColor &= DecimalToHexa6(b)
+
+        hexThree &= DecimalToHexa3(r \ 16)
+        hexThree &= DecimalToHexa3(g \ 16)
+        hexThree &= DecimalToHexa3(b \ 16)
+
+        TextBox4.Text = hexColor
+        TextBox5.Text = hexThree
+    End Sub
+
+    Private Function DecimalToHexa6(ByVal value As Integer) As String
+        Dim hex As String = ""
+
+        If value = 0 Then
+            Return "00"
+        End If
+
+        While value > 0
+            Dim remainder As Integer = value Mod 16
+            If remainder >= 10 Then
+                hex = Chr(remainder + 55) & hex
+            Else
+                hex = remainder.ToString() & hex
+            End If
+            value \= 16
+        End While
+
+        If hex.Length = 1 Then
+            hex = "0" & hex
+        End If
+
+        Return hex
+    End Function
+
+    Private Function DecimalToHexa3(ByVal value As Integer) As String
+        Dim hex As String = ""
+
+        If value = 0 Then
+            Return "0"
+        End If
+
+        While value > 0
+            Dim remainder As Integer = value Mod 16
+            If remainder >= 10 Then
+                hex = Chr(remainder + 55) & hex
+            Else
+                hex = remainder.ToString() & hex
+            End If
+            value \= 16
+        End While
+
+        Return hex
+    End Function
+
+
+
+    Private Sub TextBox4_MouseClick(sender As Object, e As MouseEventArgs) Handles TextBox4.MouseClick
+        Clipboard.SetText(TextBox4.Text)
+
+    End Sub
+
+    Private Sub TextBox4_KeyDown(sender As Object, e As KeyEventArgs) Handles TextBox4.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            Dim hexclr As String = TextBox4.Text.Replace("#", "").Trim()
+            hexclr = hexclr.ToUpper()
+
+            If hexclr.Length = 6 Then
+
+                VScrollBar1.Value = HexToDec(hexclr.Substring(0, 2))
+                VScrollBar2.Value = HexToDec(hexclr.Substring(2, 2))
+                VScrollBar3.Value = HexToDec(hexclr.Substring(4, 2))
+
+                TextBox1.Text = VScrollBar1.Value
+                TextBox2.Text = VScrollBar2.Value
+                TextBox3.Text = VScrollBar3.Value
+                Me.BackColor = Color.FromArgb(VScrollBar1.Value, VScrollBar2.Value, VScrollBar3.Value)
+
+            End If
+        End If
+
+    End Sub
+
+
+    Private Function HexToDec(ByVal rb As String) As Integer
+
+        Dim base As Integer = 1
+        Dim dec As Integer = 0
+
+        For i As Integer = rb.Length - 1 To 0 Step -1
+            Dim c As Char = rb(i)
+
+            If c >= "0" And c <= "9" Then
+                dec += (Asc(c) - Asc("0")) * base
+                base *= 16
+            ElseIf c >= "A" And c <= "F" Then
+                dec += (Asc(c) - Asc("A"c) + 10) * base
+                base *= 16
+            End If
+        Next
+
+        Return dec
+    End Function
+
+
+End Class
